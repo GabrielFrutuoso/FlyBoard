5fa788e98f9e695d54f0874f819f592ebc53f394
GabrielFrutuoso <mackbiel03@gmail.com>
2026-02-08 13:39:42 -0300

    feat: add new keys

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import { invoke } from "@tauri-apps/api/core";
 import { useState } from "react";
 import "./App.css";
+import { Key } from "./components/Key";
 
 function App() {
-
   const [activeModifiers, setActiveModifiers] = useState<string[]>([]);
   const rows = [
-    ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"],
-    ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "{"],
-    ["a", "s", "d", "f", "g", "h", "j", "k", "l", "}"],
-    ["Shift", "z", "x", "c", "v", "b", "n", "m", "Shift"],
+    ["'", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "="],
+    ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "Backspace"],
+    ["a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "]", "Enter"],
+    ["z", "x", "c", "v", "b", "n", "m", ",", ".", "/"],
+    ["Shift", "z", "x", "c", "v", "b", "n", "m"],
   ];
 
   const shiftMap: Record<string, string> = {
@@ -23,19 +24,26 @@ function App() {
     "8": "*",
     "9": "(",
     "0": ")",
-    "p": "{",
-    "l": "}"
-  };    
+    "'": "`",
+    "[": "{",
+    "]": "}",
+    ";": ":",
+    ",": "<",
+    ".": ">",
+    "/": "?",
+    "-": "_",
+    "=": "+",
+  };
 
   const getDisplayKey = (key: string) => {
-    if (key === "Backspace" || key === "Enter") return key;
+    if (key === "Backspace" || key === "Enter" || key === " ") return key;
     if (!activeModifiers.includes("Shift")) return key;
     if (shiftMap[key]) return shiftMap[key];
     return key.toUpperCase();
   };
 
   const handleKey = (key: string) => {
-    if (["Ctrl", "Shift", "Alt"].includes(key)) {
+    if (["Shift"].includes(key)) {
       setActiveModifiers((prev) =>
         prev.includes(key) ? prev.filter((k) => k !== key) : [...prev, key],
       );
@@ -49,7 +57,7 @@ function App() {
       });
     }
   };
-
+  
   return (
     <main className="flex justify-center items-center h-screen bg-zinc-950">
       <div className="flex flex-col justify-center gap-1">
@@ -58,49 +66,28 @@ function App() {
             className="w-full flex justify-center items-center gap-1"
             key={i}
           >
-            {row.map((key) => {
+            {row.map((key, index) => {
               const displayKey = getDisplayKey(key);
               const isActive = activeModifiers.includes(key);
               return (
-                <button
-                  className={`w-[60px] h-10 text-white transition-colors rounded-md ${
-                    isActive ? "bg-zinc-600" : "bg-zinc-900 hover:bg-zinc-800"
-                  }`}
-                  key={key}
-                  onMouseDown={(e) => e.preventDefault()}
+                <Key
+                  key={`${key}-${index}`}
+                  label={displayKey}
                   onClick={() => handleKey(key)}
-                >
-                  {displayKey}
-                </button>
+                  className={`${key === "Shift" || key === "Enter" ? "w-[3.75rem]" : "w-12"} h-8`}
+                  isActive={isActive}
+                />
               );
             })}
           </div>
         ))}
-        {/* <div className="flex justify-center gap-1.5">
-          {["Ctrl", "Alt"].map((key) => {
-            const isActive = activeModifiers.includes(key);
-            return (
-              <button
-                key={key}
-                className={`w-10 h-10 text-white transition-colors ${
-                  isActive ? "bg-zinc-600" : "bg-zinc-900 hover:bg-zinc-800"
-                }`}
-                onMouseDown={(e) => e.preventDefault()}
-                onClick={() => handleKey(key)}
-              >
-                {key}
-              </button>
-            );
-          })}
-        </div> */}
+
         <div className="flex justify-center">
-          <button
-            className="w-[75%] h-10 bg-zinc-900 text-white hover:bg-zinc-800 transition-colors"
-            onMouseDown={(e) => e.preventDefault()}
-            onClick={() => handleKey("return")}
-          >
-            Space
-          </button>
+          <Key
+            label="Space"
+            onClick={() => handleKey(" ")}
+            className="w-[55%] h-8"
+          />
         </div>
       </div>
     </main>
---
+++ src/components/Key.tsx
@@ -0,0 +1,45 @@
+import { ArrowBigUp, Delete, Space, CornerDownLeft } from "lucide-react";
+
+interface KeyProps {
+  label: string;
+  onClick: () => void;
+  className?: string;
+  isActive?: boolean;
+}
+
+const getKeyIcon = (label: string) => {
+  switch (label) {
+    case "Shift":
+    case "SHIFT":
+      return <ArrowBigUp size={18} />;
+    case "Backspace":
+      return <Delete size={18} />;
+    case "Space":
+      return <Space size={18} />;
+    case "Enter":
+      return <CornerDownLeft size={18} />;
+    default:
+      return null;
+  }
+};
+
+export const Key = ({
+  label,
+  onClick,
+  className = "",
+  isActive = false,
+}: KeyProps) => {
+  const icon = getKeyIcon(label);
+
+  return (
+    <button
+      className={`flex items-center justify-center text-white transition-colors rounded-md border border-zinc-800 ${className} ${
+        isActive ? "bg-zinc-700" : ""
+      }`}
+      onMouseDown={(e) => e.preventDefault()}
+      onClick={onClick}
+    >
+      {icon ? icon : label}
+    </button>
+  );
+};
